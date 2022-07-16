7f947dc85aadf6864f27369176d06da6424a6eaa
HeroMask <ian@ownby.net>
2022-07-16 15:14:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var faculty_list = [
+    {
+        "name": "Lei Li",
+        "img url": "https://ccse.kennesaw.edu/it/images/Lei%20Li.jpg",
+        "phone": "(470) 578-3915",
+        "email": "mailto:lli13@kennesaw.edu",
+        "webpage link": "https://facultyweb.kennesaw.edu/lli13/index.php",
+        "position": "Interim Chair of IT, Professor, & MSIT Coordinator",
+        "research interests": "Social Media Data Analytics, Information Security, Web Information Management, IT Education"
+    }
+]
